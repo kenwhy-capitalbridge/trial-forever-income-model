05ae7a67e1176b7181e1dcef3b98cd45a7d632ac
Kenneth Wong <kennethwong@Kenneth-Macbook-Air-M3.local>
2026-02-23 08:31:11 +0800

    allow vercel domain

--- App.tsx
+++ App.tsx
@@ -12,7 +12,7 @@ const App: React.FC = () => {
 const allowedDomains = [
   "www.thecapitalbridge.com",
   "thecapitalbridge.com",
-  "your-project-name.vercel.app",
+  "https://forever-income-calculator.vercel.app",
   "localhost"
 ];
 
